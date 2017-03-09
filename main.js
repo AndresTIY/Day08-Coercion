aec321f68394cec3b7fc48f986e4ec23d692244a
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-08 20:16:53 -0600

    table filled with data

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var row2 = document.querySelector('#row2');
 
 
 function valueCoercion (value) {
-  console.log([value, Number(value), String(value), Boolean(value)])
+  console.log([Number(value), String(value), Boolean(value)])
 }
