389f5ea3192fcdcb98160dd81cfdafd4febb8028
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-08 20:01:26 -0600

    created function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-var x = document.querySelector('#row1');
+var row1 = document.querySelector('#row1');
+var row2 = document.querySelector('#row2');
+
+
+
+function valueCoercion (value) {
+  console.log([value, Number(value), String(value), Boolean(value)])
+}
